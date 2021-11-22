cb43fd65cea0e56e3aa1aef2e62d0ac52a575a28
Anderson R da Silva <anderson.silva@ifgoiano.edu.br>
2021-11-22 15:27:02 +0100

    Add files via upload

--- codes/function_affineBrick.R
+++ codes/function_affineBrick.R
@@ -1,19 +1,13 @@
 cat("\naffineBrick(), a function to apply affine transformations ,i.e., 
   rotation and translation on a brick or raster.\n")
 
+source("https://raw.githubusercontent.com/arsilva87/hyperweeds/main/codes/function_affineCoords.R")
+
 affineBrick <- function(Brick, angle = 0, xy_shift = c(0, 0)) 
 {
    stopifnot(angle >= -360 & angle <= 360)
    if (angle != 0 & angle != 360 & angle != -360) { 
-      xy <- coordinates(Brick)
-      a <- angle*pi/180
-      rot <- matrix(c(cos(a), -sin(a), sin(a), cos(a)), nrow = 2)
-      cen <- colMeans(xy)
-      rxy <- sweep(sweep(xy, 2, cen) %*% rot, 
-         2, cen + xy_shift, FUN = "+")
-      rxy <- as.data.frame(rxy)
-      colnames(rxy) <- c("x", "y")
-      coordinates(rxy) <- ~x+y
+      rxy <- affineCoords(Brick, angle, xy_shift)
       rb <- raster::brick()
       extent(rb) <- extent(rxy)
       crs(rb) <- crs(Brick)
